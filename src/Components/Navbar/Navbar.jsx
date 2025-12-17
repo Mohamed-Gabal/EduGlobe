87bca41a56febe0cca6d886155ae2c2611343965
Mohamed Gabal <abogabal672@gmail.com>
2025-12-17 17:29:10 +0200

    edit on menu for the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,34 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/Image/logo_head.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { GrMenu } from "react-icons/gr";
+import { IoClose } from "react-icons/io5";
 import "./Navbar.css";
 
 const Navbar = () => {
+  
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <nav className="nav">
-      {/* Left Section: Logo */}
+      {/* Logo */}
       <div className="nav_left">
-        <Link to="/"><img src={Logo} alt="EduGlobe Logo" className="nav_logo_img" /></Link>
-        <div className="nav_logo_text">
-          EduGlobe
-          <p>Language Centre</p>
-        </div>
+        <Link to="/" className="nav_logo">
+          <img src={Logo} alt="EduGlobe Logo" className="nav_logo_img" />
+          <div className="nav_logo_text">
+            EduGlobe
+            <p>Language Centre</p>
+          </div>
+        </Link>
       </div>
 
-      {/* Center Section: Links */}
-      <ul className="nav_links">
-        <Link to="/">Home</Link>
-        <Link to="/courses">Courses</Link>
-        <Link to="/apply_now">Apply Now</Link>
-        <Link to="/contact">Contact</Link>
+      {/* Menu Icon (Mobile) */}
+      <button
+        className="nav_menu_btn"
+        onClick={() => setMenuOpen(!menuOpen)}
+        aria-label="Toggle Menu"
+      >
+        {menuOpen ? <IoClose /> : <GrMenu />}
+      </button>
+
+      {/* Links */}
+      <ul className={`nav_links ${menuOpen ? "nav_links--active" : ""}`}>
+        <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
+        <li><Link to="/courses" onClick={() => setMenuOpen(false)}>Courses</Link></li>
+        <li><Link to="/apply_now" onClick={() => setMenuOpen(false)}>Apply Now</Link></li>
+        <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
       </ul>
 
-      {/* Right Section: Buttons */}
+      {/* Actions (Desktop Only) */}
       <div className="nav_actions">
         <button className="btn_signin">Sign In</button>
-        <Link to="/contact"><button className="btn_apply">Apply Now</button></Link>
+        <Link to="/contact">
+          <button className="btn_apply">Apply Now</button>
+        </Link>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
